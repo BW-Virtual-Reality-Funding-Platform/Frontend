13535d119e3366a8aaf52539a3d7b8bf06b5194c
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-07-24 14:36:29 -0500

    Create New Project and Login forms are rendering with very basic state in place

--- my-app/src/components/NewProjectForm.js
+++ my-app/src/components/NewProjectForm.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
+import * as ReactBootstrap from "react-bootstrap";
+import { InputGroup, FormControl, Form, Button } from "react-bootstrap";
 import axios from "axios";
+import "./NewProjectForm.css";
 
 export default function NewProjectForm() {
   const [formState, setFormState] = useState({
@@ -7,26 +10,93 @@ export default function NewProjectForm() {
     projectDescription: "",
     goalAmount: "",
     amountReceived: "",
-    fundingCompleted: "", 
+    fundingCompleted: "",
   });
   return (
-    <form>
-      <label htmlFor="projectTitle">
-        <input type="text" name="projectTitle" value={formState.projectTitle}></input>
-      </label>
-      <label htmlFor="projectDescription">
-        <textarea type="text" name="projectDescription" value={formState.projectDescription}></textarea>
-      </label>
-      <label htmlFor="goalAmount">
-        <input type="text" name="goalAmount" value={formState.goalAmount}></input>
-      </label>
-      <label htmlFor="amountReceived">
-        <input type="text" name="amountReceived" value={formState.amountReceived}></input>
-      </label>
-      <label htmlFor="fundingCompleted">
-        <input type="text" name="fundingCompleted" value={formState.fundingCompleted}></input>
-      </label>
-      <label></label>
-    </form>
+    <div>
+      <ReactBootstrap.Form>
+        <Form className="project-form">
+          <h1>Create New Project</h1>
+          {/* <label>Enter Username</label>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup> */}
+
+          <label>Project Title</label>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Project Title"
+              aria-label="Project Title"
+              aria-describedby="basic-addon2"
+              type="text"
+              name="Project Title"
+              value={formState.projectTitle}
+            />
+            <InputGroup.Append>
+              <InputGroup.Text id="basic-addon2">PT</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <label>Project Description</label>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>PD</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              as="textarea"
+              aria-label="With textarea"
+              type="text"
+              name="Project Description"
+              value={formState.projectDescription}
+            />
+          </InputGroup>
+
+          <label>Goal Amount</label>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Amount (to the nearest dollar)"
+              type="text"
+              name="Goal Amount"
+              value={formState.goalAmount}
+            />
+            <InputGroup.Append>
+              <InputGroup.Text>.00</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <label>Amount Received</label>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Amount (to the nearest dollar)"
+              type="text"
+              name="Amount Received"
+              value={formState.amountReceived}
+            />
+            <InputGroup.Append>
+              <InputGroup.Text>.00</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <br></br>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </ReactBootstrap.Form>
+    </div>
   );
 }
