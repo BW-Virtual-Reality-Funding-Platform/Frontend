936ee789b3eb552eb239aba8722f8e80264648b9
damico williams <damicowilliams@gmail.com>
2020-07-24 19:11:55 -0700

    auth code should work after getting with backend. Can type in text field with bootstrap

--- my-app/src/components/LoginForm.js
+++ my-app/src/components/LoginForm.js
@@ -28,9 +28,10 @@ useEffect(() => {
 
 const handleSubmit = e => {
     e.preventDefault()
+    console.log(loginState)
 
     axiosWithAuth()
-      .post('/api/auth/login', loginState)
+      .post('/auth/login', loginState)
       .then(res => {
         console.log(res);
         localStorage.setItem('token', res.data.payload);
@@ -38,59 +39,47 @@ const handleSubmit = e => {
         history.push('/browse')
       })
       .catch(err => {
-        console.log(err);
+        console.log(err.message);
       });
   };
   return (
-  <div>
-      <ReactBootstrap.Form>
-        <Form className="login-form">
-          <h1>Login</h1>
-          <Form.Group controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="Username"
-              placeholder="Enter Username"
-              name="Username"
-              value={loginState.username}
-            />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
+    <div>
+    <Form className="login-form" onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <Form.Group controlId="formBasicUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          value={loginState.username}
+          onChange={handleChange}
+        />
+        
+        <Form.Text className="text-muted"></Form.Text>
+      </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={loginState.password}/>
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </ReactBootstrap.Form>
-    </div>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Password"
+          name="password"
+          value={loginState.password}
+          onChange={handleChange}
+        />
+        
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+     
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  </div>
   );
 }
 
 
-//ignore or delete but this is a working form to test authentication
-//Authentication not working yet
-{/* <div>
-<h1>Your Here!</h1>
-<form onSubmit={handleSubmit}>
-        <input
-        type='text'
-        name='username'
-        value={loginState.username}
-        onChange={handleChange}
-        />
-        <input
-        type='password'
-        name='password'
-        value={loginState.password}
-        onChange={handleChange}
-        />
-        <button>Log in</button>
-    </form>
-</div> */}
